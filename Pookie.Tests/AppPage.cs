d16c319a75cfd8fed185d6cede228c6575f66ac2
Derek Cacciotti <applegamer247@gmail.com>
2022-10-08 16:52:48 -0400

    Added EntryPoint and appPage

---
+++ Pookie.Tests/AppPage.cs
@@ -0,0 +1,19 @@
+﻿using AFUT.Tests.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFUT.Tests
+{
+    public class AppPage
+    {
+        private readonly IPookieWebDriver _driver;
+
+        public AppPage(IPookieWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
---
+++ Pookie.Tests/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using AFUT.Tests.Config;
+using AFUT.Tests.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFUT.Tests
+{
+    public static class EntryPoint
+    {
+        public static AppPage OpenPage(IPookieWebDriver driver, IAppConfig config)
+        {
+            if (!string.IsNullOrEmpty(config.UserName))
+            {
+                driver.Login(config.UserName, config.Password, config.AppUrl);
+            }
+            else
+            {
+                driver.Navigate().GoToUrl(config.AppUrl);
+            }
+            return new AppPage(driver);
+        }
+    }
+}
